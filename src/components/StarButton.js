9f12e9e5e7517a210f454af5db1852b30254af5e
watanabehideki <hideki.art.2280@gmail.com>
2021-11-23 17:21:26 +0900

    add star

--- src/components/StarButton.js
+++ src/components/StarButton.js
@@ -1,13 +1,27 @@
 import React from "react"
+import { Mutation } from "react-apollo"
+import { ADD_STAR } from "../graphql"
 
 export const StarButton = ({ node }) => {
   const totalCount = node.stargazers.totalCount
   const viewerHasStarred = node.viewerHasStarred
   const starOrStars = totalCount === 1 ? "star" : "stars"
+
+  const StarStatus = ({ addStar }) => {
+    return (
+      <button
+        onClick={() => {
+          addStar({ variables: { input: { starrableId: node.id } } })
+        }}
+      >
+        {totalCount}
+        {starOrStars} | {viewerHasStarred ? "starred" : "-"}
+      </button>
+    )
+  }
   return (
-    <button>
-      {totalCount}
-      {starOrStars} | {viewerHasStarred ? "starred" : "-"}
-    </button>
+    <Mutation mutation={ADD_STAR}>
+      {(addStar) => <StarStatus addStar={addStar} />}
+    </Mutation>
   )
 }
